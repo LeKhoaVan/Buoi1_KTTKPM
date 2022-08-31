43e7d2982a7c8f4a7d4521407449d3d3f2a5ccd5
LeKhoaVan <lekhoavan325802@gmail.com>
2022-08-31 09:20:51 +0700

    bản hoàn thiện

--- src/helper/XMLConvert.java
+++ src/helper/XMLConvert.java
@@ -1,4 +1,5 @@
 package helper;
+
 import java.io.StringReader;
 import java.io.StringWriter;
 import javax.xml.bind.JAXBContext;
@@ -7,20 +8,23 @@ import javax.xml.bind.Unmarshaller;
 
 public class XMLConvert<T> {
 	private T type;
+
 	public XMLConvert(T type) {
 		this.type = type;
 	}
+
 	@SuppressWarnings("all")
-	public T xml2Object(String xml) throws Exception{
-		T sv=null;
-		JAXBContext ctx= JAXBContext.newInstance(type.getClass());
+	public T xml2Object(String xml) throws Exception {
+		T sv = null;
+		JAXBContext ctx = JAXBContext.newInstance(type.getClass());
 		Unmarshaller ms = ctx.createUnmarshaller();
-		sv=(T) ms.unmarshal(new StringReader(xml));
+		sv = (T) ms.unmarshal(new StringReader(xml));
 		return sv;
 	}
-	public String object2XML(T obj)throws Exception{
-		JAXBContext ctx= JAXBContext.newInstance(type.getClass());
-		Marshaller ms=ctx.createMarshaller();
+
+	public String object2XML(T obj) throws Exception {
+		JAXBContext ctx = JAXBContext.newInstance(type.getClass());
+		Marshaller ms = ctx.createMarshaller();
 		StringWriter sw = new StringWriter();
 		ms.marshal(obj, sw);
 		return sw.toString();
